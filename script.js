91d27df606e1c4366d6c31a12a4ebd6773e1d816
GittyTham <oliver.tham@student.chasacademy.se>
2026-09-11 14:42:09 +0200

    easter egg

--- script.js
+++ script.js
@@ -113,3 +113,9 @@ clickBtn.addEventListener("click", () => {
   // Uppdatera texten på sidan med den nya siffran
   countDisplay.textContent = count;
 });
+// Easter egg
+document.addEventListener("keydown", function (event) {
+  if (event.key.toLowerCase() === "r") {
+    document.getElementById("rat").style.display = "block";
+  }
+});
